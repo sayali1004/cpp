5475bd7633410c33c252c5203805c59414b213d7
Sayali Shelke <57850705+sayali1004@users.noreply.github.com>
2022-12-30 08:16:17 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int A[4];
+    int B[4]={0};
+    int C[4]={1,5,8,9};
+    int D[4]={1,2,3};
+    
+    for(int i=0;i<4;i++)
+      {printf( "%d ", D[i] );
+      
+      }
+
+    return 0;
+}
